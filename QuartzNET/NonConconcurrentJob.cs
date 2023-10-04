1f541b7987ce8d5a7678fcd4273ae2830d48be7e
figueira.joao <79715612+joaoafigueira@users.noreply.github.com>
2023-10-04 13:37:08 -0300

    Quartz.NET POC

---
+++ QuartzNET/NonConconcurrentJob.cs
@@ -0,0 +1,22 @@
+﻿using Quartz;
+
+namespace QuartzNET
+{
+    [DisallowConcurrentExecution]
+    public class NonConconcurrentJob : IJob
+    {
+        private readonly ILogger<NonConconcurrentJob> _logger;
+
+        public NonConconcurrentJob(ILogger<NonConconcurrentJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogWarning("Executing NonConconcurrentJob every 5 seconds...");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ QuartzNET/Program.cs
@@ -0,0 +1,42 @@
+using Quartz;
+using QuartzNET;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddQuartz(options =>
+{
+    // Create a key for the job
+    var nonConconcurrentJob = new JobKey("NonConconcurrentJob");
+
+    // Registers the job in the dependency injection container.
+    options.AddJob<NonConconcurrentJob>(opts => opts.WithIdentity(nonConconcurrentJob));
+
+    // Creates a trigger that fires the "NonConconcurrentJob" job every 5 seconds, forever.
+    options.AddTrigger(opts => opts
+               .ForJob(nonConconcurrentJob)
+               .WithIdentity("NonConconcurrentTrigger")
+               .StartNow()
+               .WithSimpleSchedule(x => x
+                            .WithIntervalInSeconds(5)
+                            .RepeatForever()
+                            .Build()));
+});
+
+
+// Add a Hosted Quartz Server into process that will be started and stopped based on applications lifetime.
+
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
